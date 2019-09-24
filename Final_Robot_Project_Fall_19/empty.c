fe61f4cb1ed1f10618e702ae92cb571c470376b0
Fazal Mahmood <fazalmahmood@Fazals-MacBook-Pro.local>
2019-09-23 21:08:14 -0500

    Added Milestone 2. Only bluetooth module configured to UART, no commands used

--- Final_Robot_Project_Fall_19/empty.c
+++ Final_Robot_Project_Fall_19/empty.c
@@ -50,6 +50,55 @@
 /* Board Header file */
 #include "Board.h"
 
+/* Used to include "defines" */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+
+
+
+
+
+
+/*
+ * ======= Helper Functions =======
+ * Milestone 2: Configure_UART1()
+ */
+void Configure_UART1() // Connection for the Bluetooth
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);// Enable the UART 1 module
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);// Enable the GPIO B peripheral
+
+    GPIOPinConfigure(GPIO_PB0_U1RX); // Enable UART3 function on GPIO Port B pins 6
+    GPIOPinConfigure(GPIO_PB1_U1TX); // Enable UART3 function on GPIO Port B pins 7
+    GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1); // Configure GPIO Port C to pins 6 and 7 to be used as UART
+
+    // Initialize the UART. Set the baud rate, number of dataa bits, turn off
+    // parity, number of stop bits, and stick mode. The UART is enabled by the function call
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200,
+            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+}
+
+/*
+ *  ======== configureBluetooth ========
+ *  Setup bluetooth module using PB0 = Tx and PB1 = Rx
+ *  Milestone 2
+ */
+Void configureBluetooth(UArg arg0, UArg arg1)
+{
+    //Set CPU Clock to 40MHz. 400MHz PLL/2 = 200 DIV 5 = 40MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    Configure_UART1();
+
+    // Enables port, sets pins 1-3 (RGB) pins for output
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+}
 
 /*
  *  ======== heartBeatFxn ========
@@ -67,13 +116,14 @@ Void heartBeatFxn(UArg arg0, UArg arg1)
     }
 }
 
+
+
+
 /*
  *  ======== main ========
  */
 int main(void)
 {
-    Task_Params taskParams;
-
     /* Call board init functions */
     Board_initGeneral();
     Board_initGPIO();
